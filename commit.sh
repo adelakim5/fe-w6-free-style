7f06dae897f1de2d231b6de9bf841f1ebbf7a63e
adelakim <diddnjs02@likelion.org>
2021-03-19 09:01:34 +0900

    commit test

--- commit.sh
+++ commit.sh
@@ -3,8 +3,12 @@ read
 git init
 git add .
 <<<<<<< HEAD
+<<<<<<< HEAD
 git commit -m "$REPLY"
 =======
 git commit -m "commit"
 >>>>>>> 88d12ba ([Chore] git rebase & update registerNewUser)
+=======
+git commit -m "$REPLY"
+>>>>>>> 2466fcf (commit test)
 git push origin web2
