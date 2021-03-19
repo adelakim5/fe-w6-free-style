62a420795c0919be65a12e9ff407a8aae912b4c7
adelakim <diddnjs02@likelion.org>
2021-03-19 09:00:58 +0900

    [Chore] git rebase & update registerNewUser

--- commit.sh
+++ commit.sh
@@ -2,5 +2,9 @@ echo -e "commit -m "
 read
 git init
 git add .
+<<<<<<< HEAD
 git commit -m "$REPLY"
+=======
+git commit -m "commit"
+>>>>>>> 88d12ba ([Chore] git rebase & update registerNewUser)
 git push origin web2
